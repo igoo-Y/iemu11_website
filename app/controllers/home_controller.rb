5c97d0ab483d0136ab32350f6060fa3d945d5468
igoo-Y <yang_ig@naver.com>
2021-04-30 18:59:56 +0900

    4/30

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,4 +1,14 @@
 class HomeController < ApplicationController
   def index
+    # @volunteer = Volunteer.find(params[:id])
+    # @post = Post.find(params[:id])
   end
+
+  # def post_params
+  #   params.require(:post).permit(:title)
+  # end
+  #
+  # def volunteer_params
+  #   params.require(:volunteer).permit(:title)
+  # end
 end
